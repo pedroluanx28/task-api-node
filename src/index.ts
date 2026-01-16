61bbe7de9d2acc327b4a15b166ec22e63e7f5142
Pedro Luan <126918758+pedroluanx28@users.noreply.github.com>
2026-01-15 23:30:22 -0300

    Update README.md

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ app.use(express.json());
 app.use("/api/tasks", taskRoutes);
 
 app.listen(3001, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3001");
 });
